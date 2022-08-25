f6926cc1fe96fcfa6beaab1953c6fbe63df48dc5
Paulaxam <paulacudicio@gmail.com>
2022-08-25 11:53:04 -0300

    Fusión de clase7 a index. Agregado de toogle al menu descktop

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let emergentMobileMenu = document.querySelector('.mobile-menu');
 let menuBurgerIcon = document.querySelector('.burger-menu');
+let emergentDesktopMenu = document.querySelector('.desktop-menu');
+let userEmail = document.querySelector('.navbar-email');
 
 menuBurgerIcon.addEventListener('click', toggleEmergentMobileMenu);
 
 function toggleEmergentMobileMenu() {
     emergentMobileMenu.classList.toggle('inactive');
+}
+
+userEmail.addEventListener('click', toogleEmergentDesktopMenu);
+
+function toogleEmergentDesktopMenu() {
+    emergentDesktopMenu.classList.toggle('inactive');
 }
